fa0a1defeac295341c3a9fcb6a4aef282132c44b
Doris Li <bubucc0629@gmail.com>
2022-05-01 01:50:51 +0800

    修改Homepage

--- src/pages/HomePage/firstpage.js
+++ src/pages/HomePage/firstpage.js
@@ -1,11 +1,10 @@
 import React from "react";
-
-import Clock from "./components/Clock.js";
+import Title from "./components/Title.js";
 import "./firstpage.css";
 const Firstpage = function () {
   return (
     <div className="home">
-      <Clock />
+      <Title />
     </div>
   );
 };
